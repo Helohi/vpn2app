ceb848b1144a2138e8a001a39aa3cfcb0535e8c6
Helohi <helomegido@gmail.com>
2025-03-31 00:21:44 +0300

    Flutter new version update

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/yhlas/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/yhlas/Projects/Flutter/vpn2app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=2.5.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
